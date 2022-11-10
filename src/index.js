7fc9dd5255ac0b2617e9860aaeddf89b4dde91d4
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2022-11-10 23:17:13 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,47 +1,71 @@
 const axios = require('axios').default;
 import Notiflix from 'notiflix';
+const BASE_URL = 'https://pixabay.com/api/';
 const ACES_KEY = '30502638-8236fb6cc30a79f817dee13c3';
 
-function fetchFromUser() {
+const getEl = element => document.querySelector(element);
+const form = getEl('#search-form');
+
+let searchQuery = '';
+
+form.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick(e) {
+  e.preventDefault();
+  searchQuery = e.currentTarget.elements.searchQuery.value;
+  return fetchFromUser(searchQuery);
+}
+
+function fetchFromUser(searchQuery) {
   return axios
-    .get('https://pixabay.com/api/', {
+    .get(`${BASE_URL}`, {
       params: {
         key: ACES_KEY,
-        q: 'cat',
+        q: `${searchQuery}`,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
+        per_page: 50,
       },
     })
     .then(response => {
       return response.data;
     })
-    .then(result => result.hits);
+    .then(result => {
+      Notiflix.Notify.info(`"Hooray! We found ${result.totalHits} images." `);
+      return result.hits;
+    })
+    .then(r => {
+      console.log(r);
+    });
 }
 
-fetchFromUser()
-  .then(function destr(res) {
-    res.forEach(element => console.log(element));
-  })
-  
+// fetchFromUser()
+//   .then(res => res.json()).then(data => console.log(data))
 
-// function makeCardElement(element) {
-//   const cardMarkup = `<div class="photo-card">
-//                     <img src="" alt="" loading="lazy" />
-//                     <div class="info">
-//                     <p class="info-item">
-//                         <b>Likes </b>
-//                     </p>
-//                     <p class="info-item">
-//                       <b>Views:</b>
-//                     </p>
-//                     <p class="info-item">
-//                       <b>Comments:</b>
-//                     </p>
-//                     <p class="info-item">
-//                       <b>Downloads: </b>
-//                     </p>
-//                     </div>
-//                     </div>`;
- 
-// }
+// fetchFromUser('cat');
+// fetchFromUser('dog');
+// fetchFromUser('sun');
+
+function makeCardElement(element) {
+  const cardMarkup = `<div class="photo-card">
+                    <img src="" alt="" loading="lazy" />
+                    <div class="info">
+                    <p class="info-item">
+                        <b>Likes </b>
+                    </p>
+                    <p class="info-item">
+                      <b>Views:</b>
+                    </p>
+                    <p class="info-item">
+                      <b>Comments:</b>
+                    </p>
+                    <p class="info-item">
+                      <b>Downloads: </b>
+                    </p>
+                    </div>
+                    </div>`;
+  document
+    .querySelector('.gallery')
+    .insertAdjacentElement('beforeend', cardMarkup);
+}
