8a0d7ce9e6b07e65899d059042cb8b29b9d20af4
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2022-11-15 00:02:45 +0200

    async remastered

--- src/index.js
+++ src/index.js
@@ -7,46 +7,49 @@ const getEl = element => document.querySelector(element);
 const form = getEl('#search-form');
 
 let searchQuery = '';
+let currentPage = 1;
 
 form.addEventListener('submit', onSubmitClick);
 
-function onSubmitClick(e) {
+async function onSubmitClick(e) {
   e.preventDefault();
 
   document.querySelector('.gallery').innerHTML = '';
   searchQuery = e.currentTarget.elements.searchQuery.value;
-
-  fetchFromUser(searchQuery).then(data => renderMarkup(data));
+  // fetchFromUser(searchQuery);
+  axionFetchFromUser(searchQuery).then(r => renderMarkup(r));
+  e.target.reset();
+  currentPage = 1;
 }
 
-function fetchFromUser(searchQuery) {
-  return axios
-    .get(`${BASE_URL}`, {
-      params: {
-        key: ACES_KEY,
-        q: `${searchQuery}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        per_page: 50,
-      },
-    })
-    .then(response => {
-      return response.data;
-    })
-    .then(result => {
-      if (result.totalHits === 0) {
-        Notiflix.Notify.warning('eeerr');
-      } else {
-        Notiflix.Notify.info(`"Hooray! We found ${result.totalHits} images." `);
-      }
-      return result.hits;
-    })
-    .then(hits => {
-      console.log(hits);
-      return hits;
-    });
-}
+// function fetchFromUser(searchQuery) {
+//   currentPage += 1;
+//   return axios
+//     .get(`${BASE_URL}`, {
+//       params: {
+//         key: ACES_KEY,
+//         q: `${searchQuery}`,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: 'true',
+//         per_page: 50,
+//         page: `${currentPage}`,
+//       },
+//     })
+//     .then(response => {
+//       return response.data;
+//     })
+//     .then(result => {
+//       if (result.totalHits === 0) {
+//         Notiflix.Notify.warning('eeerr');
+//       } else {
+//         Notiflix.Notify.info(`"Hooray! We found ${result.totalHits} images." `);
+//       }
+
+//       return result.hits;
+//     })
+//     .then(data => renderMarkup(data));
+// }
 
 function renderMarkup(data) {
   const markup = data
@@ -80,4 +83,46 @@ function renderMarkup(data) {
     })
     .join('');
   document.querySelector('.gallery').insertAdjacentHTML('beforeend', markup);
+}
+
+window.addEventListener('scroll', async () => {
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+  if (scrollTop === scrollHeight - clientHeight) {
+    axionFetchFromUser(searchQuery).then(r => renderMarkup(r));
+  }
+});
+
+async function axionFetchFromUser(searchQuery) {
+  try {
+    const fetchResponse = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: ACES_KEY,
+        q: `${searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 50,
+        page: `${currentPage}`,
+      },
+    });
+    const dataCollection = await fetchResponse.data;
+    if (fetchResponse.data.totalHits !== 0) {
+      Notiflix.Notify.info(
+        `"Hooray! We found ${fetchResponse.data.totalHits} images." `
+      );
+
+      currentPage += 1;
+    } else {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    const collection = await dataCollection.hits;
+    return collection;
+  } catch {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
---
+++ src/js/axion-fethc.js
@@ -0,0 +1,35 @@
+async function axionFetchFromUser(searchQuery) {
+  try {
+    const fetchResponse = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: ACES_KEY,
+        q: `${searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 50,
+        page: `${currentPage}`,
+      },
+    });
+    const dataCollection = await fetchResponse.data;
+    if (fetchResponse.data.totalHits !== 0) {
+      Notiflix.Notify.info(
+        `"Hooray! We found ${fetchResponse.data.totalHits} images." `
+      );
+
+      currentPage += 1;
+    } else {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    const collection = await dataCollection.hits;
+    return collection;
+  } catch {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    return;
+  }
+}
