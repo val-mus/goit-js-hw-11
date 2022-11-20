32e476a9df8c63f94e8a244da1e23ba572f90550
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2022-11-20 15:40:09 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-const axios = require('axios').default;
-import axios from 'axios';
-import Notiflix from 'notiflix';
 
-// import axionFetchFromUser from './js/axionFetch';
+import axionFetchFromUser from './js/axionFetch';
 import renderMarkup from './js/renderMarkup';
 
 const BASE_URL = 'https://pixabay.com/api/';
@@ -12,7 +9,7 @@ const getEl = element => document.querySelector(element);
 const form = getEl('#search-form');
 
 let searchQuery = '';
-let currentPage = 1;
+
 
 form.addEventListener('submit', onSubmitClick);
 
@@ -26,89 +23,17 @@ async function onSubmitClick(e) {
     currentPage = 1;
   });
 
-  const markupData = await axionFetchFromUser(searchQuery);
+  const markupData = await axionFetchFromUser(searchQuery, BASE_URL, ACES_KEY);
   renderMarkup(markupData);
 
   e.target.reset();
 }
 
-// function renderMarkup(data) {
-//   const markup = data
-//     .map(item => {
-//       const {
-//         webformatURL,
-//         tags,
-//         likes,
-//         largeImageURL,
-//         views,
-//         comments,
-//         downloads,
-//       } = item;
-//       return `<div class="photo-card">
-//     <img src="${webformatURL}" alt="${tags}" loading="lazy" class="photo-card__img"/>
-//     <div class="info">
-//     <p class="info-item">
-//         <b>Likes: ${likes} </b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views: ${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments: ${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads: ${downloads}</b>
-//     </p>
-//     </div>
-//     </div>`;
-//     })
-//     .join('');
-//   document.querySelector('.gallery').insertAdjacentHTML('beforeend', markup);
-// }
-
 window.addEventListener('scroll', async () => {
   const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
   if (scrollTop === scrollHeight - clientHeight) {
-    const markupData = await axionFetchFromUser(searchQuery);
+    const markupData = await axionFetchFromUser(searchQuery, BASE_URL, ACES_KEY);
     renderMarkup(markupData);
   }
 });
 
-async function axionFetchFromUser(searchQuery) {
-  try {
-    const fetchResponse = await axios.get(`${BASE_URL}`, {
-      params: {
-        key: ACES_KEY,
-        q: `${searchQuery}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        per_page: 50,
-        page: `${currentPage}`,
-      },
-    });
-
-    const dataCollection = await fetchResponse.data;
-
-    if (fetchResponse.data.totalHits !== 0) {
-      if (currentPage === 1) {
-        Notiflix.Notify.info(
-          `"Hooray! We found ${fetchResponse.data.totalHits} images." `
-        );
-      }
-      currentPage += 1;
-    } else {
-      Notiflix.Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-    const collection = await dataCollection.hits;
-    return collection;
-  } catch {
-    Notiflix.Notify.warning(
-      "We're sorry, but you've reached the end of search results."
-    );
-    return;
-  }
-}
--- src/js/axionFetch.js
+++ src/js/axionFetch.js
@@ -1,41 +1,48 @@
-// const axios = require('axios').default;
-// import axios from 'axios';
-// import Notiflix from 'notiflix';
+const axios = require('axios').default;
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const ACES_KEY = '30502638-8236fb6cc30a79f817dee13c3';
+let currentPage = 1;
 
-// export default async function axionFetchFromUser(searchQuery) {
-//   try {
-//     const fetchResponse = await axios.get(`${BASE_URL}`, {
-//       params: {
-//         key: ACES_KEY,
-//         q: `${searchQuery}`,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: 'true',
-//         per_page: 50,
-//         page: `${currentPage}`,
-//       },
-//     });
-//     const dataCollection = await fetchResponse.data;
+export default async function axionFetchFromUser(
+  searchQuery,
+  BASE_URL,
+  ACES_KEY
+) {
+  try {
+    const fetchResponse = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: ACES_KEY,
+        q: `${searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 50,
+        page: `${currentPage}`,
+      },
+    });
 
-//     if (fetchResponse.data.totalHits !== 0) {
-//       Notiflix.Notify.info(
-//         `"Hooray! We found ${fetchResponse.data.totalHits} images." `
-//       );
-//       currentPage += 1;
-//     } else {
-//       Notiflix.Notify.warning(
-//         'Sorry, there are no images matching your search query. Please try again.'
-//       );
-//       return;
-//     }
-//     const collection = await dataCollection.hits;
-//     return collection;
-//   } catch {
-//     Notiflix.Notify.warning(
-//       "We're sorry, but you've reached the end of search results."
-//     );
-//   }
-// }
+    const dataCollection = await fetchResponse.data;
+
+    if (fetchResponse.data.totalHits !== 0) {
+      if (currentPage === 1) {
+        Notiflix.Notify.info(
+          `"Hooray! We found ${fetchResponse.data.totalHits} images." `
+        );
+      }
+      currentPage += 1;
+    } else {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    const collection = await dataCollection.hits;
+    return collection;
+  } catch {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    return;
+  }
+}
